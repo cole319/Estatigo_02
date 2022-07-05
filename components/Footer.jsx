0716a380a1ed6b5dc45a2cbc9c8d1536d2476808
Suryarghya <suryar1332001@gmail.com>
2022-07-06 00:01:48 +0530

    initial commit

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+import { Box } from "@chakra-ui/layout";
+
+const Footer = () => (
+  <Box
+    textAlign="center"
+    p="5"
+    color="gray.600"
+    borderTop="1px"
+    paddingTop="40"
+    borderColor="gray.100"
+  >
+    © 2022 EstatiGO, Inc.
+  </Box>
+);
+
+export default Footer;
